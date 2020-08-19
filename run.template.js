5194fe7432b946503a14759b04273c0f90033b71
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-08-19 16:24:28 +0800

    Added proper support for short shell parameter.

--- run.template.js
+++ run.template.js
@@ -1,46 +1,164 @@
 #! /usr/bin/env node
 
-const SAMPLE_SHELL_PARAMETER = (
-	"--sampleParameter"
-);
-
 (
 	async	function run$ModuleVariableTitleNamespace( shellParameterList ){
 				"use strict";
+
+				//; @template-engine-code-space;
+				const checkShellParameter = (
+					function checkShellParameter( shellParameter, shortShellParameter ){
+						return	(
+									[
+										(
+												shellParameterList
+												.includes(
+													(
+														shortShellParameter
+													)
+												)
+											===	true
+										),
+
+										(
+												shellParameterList
+												.includes(
+													(
+														shellParameter
+													)
+												)
+											===	true
+										),
+									]
+									.some(
+										(
+											( status ) => ( status )
+										)
+									)
+								);
+					}
+				);
+
+				const getShellParameterValue = (
+					function getShellParameterValue( shellParameter, shortShellParameter ){
+						if(
+								(
+										checkShellParameter(
+											(
+												shellParameter
+											),
+
+											(
+												shortShellParameter
+											)
+										)
+									===	true
+								)
+						){
+							return	(
+										shellParameterList[
+											(
+												[
+													(
+														shellParameterList
+														.indexOf(
+															(
+																shellParameter
+															)
+														)
+													),
+
+													(
+														shellParameterList
+														.indexOf(
+															(
+																shortShellParameter
+															)
+														)
+													),
+												]
+												.filter(
+													(
+														( index ) => (
+																index
+															>=	0
+														)
+													)
+												)
+												.pop( )+1
+											)
+										]
+									);
+						}
+						else{
+							return	(
+										undefined
+									);
+						}
+					}
+				);
+
+				const resolveShellParameterValue = (
+					function resolveShellParameterValue( shellParameter, shortShellParameter, defaultValue ){
+						return	(
+										(
+												(
+														checkShellParameter(
+															(
+																shellParameter
+															),
+
+															(
+																shortShellParameter
+															)
+														)
+													===	true
+												)
+										)
+									?	(
+											getShellParameterValue(
+												(
+													shellParameter
+												),
+
+												(
+													shortShellParameter
+												)
+											)
+										)
+									:	(
+											defaultValue
+										)
+								);
+					}
+				);
+				//;	@end-template-engine-code-space;
+
+				const SAMPLE_SHELL_PARAMETER = (
+					"--sampleParameter"
+				);
+
+				const SAMPLE_SHORT_SHELL_PARAMETER = (
+					"--sp"
+				);
 
 				const $moduleVariableNamespace = (
 					require( "./{{ @module-value-namespace }}.js" )
 				);
 
 				const sampleParameter = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												SAMPLE_SHELL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												SAMPLE_SHELL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							SAMPLE_SHELL_PARAMETER
+						),
+
+						(
+							SAMPLE_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				return	(
