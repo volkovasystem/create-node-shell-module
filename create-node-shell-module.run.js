64db8ee1e15c74f8f3cec15bb17a1d2329033859
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-08-19 16:15:54 +0800

    Added proper support for short shell parameter.

--- create-node-shell-module.run.js
+++ create-node-shell-module.run.js
@@ -1,222 +1,285 @@
 #! /usr/bin/env node
 
-const MODULE_DIRECTORY_PATH_SHELL_PARAMETER = (
-	"--moduleDirectoryPath"
-);
-
-const MODULE_VALUE_NAMESPACE_PARAMETER = (
-	"--moduleValueNamespace"
-);
-
-const MODULE_SCOPE_SHELL_PARAMETER = (
-	"--moduleScope"
-);
-
-const MODULE_DESCRIPTION_SHELL_PARAMETER = (
-	"--moduleDescription"
-);
-
-const AUTHOR_TITLE_NAMESPACE_SHELL_PARAMETER = (
-	"--authorTitleNamespace"
-);
-
-const AUTHOR_CONTACT_DETAIL_PARAMETER = (
-	"--authorContactDetail"
-);
-
 (
 	async	function runCreateNodeShellModule( shellParameterList ){
 				"use strict";
+
+				//; @template-engine-code-space;
+				const checkShellParameter = (
+					function checkShellParameter( shellParameter, shortShellParameter ){
+						return	(
+									[
+										(
+												shellParameterList
+												.includes(
+													(
+														shortShellParameter
+													)
+												)
+											===	true
+										),
+
+										(
+												shellParameterList
+												.includes(
+													(
+														shellParameter
+													)
+												)
+											===	true
+										),
+									]
+									.some(
+										(
+											( status ) => ( status )
+										)
+									)
+								);
+					}
+				);
+
+				const getShellParameterValue = (
+					function getShellParameterValue( shellParameter, shortShellParameter ){
+						if(
+								(
+										checkShellParameter(
+											(
+												shellParameter
+											),
+
+											(
+												shortShellParameter
+											)
+										)
+									===	true
+								)
+						){
+							return	(
+										shellParameterList[
+											(
+												[
+													(
+														shellParameterList
+														.indexOf(
+															(
+																shellParameter
+															)
+														)
+													),
+
+													(
+														shellParameterList
+														.indexOf(
+															(
+																shortShellParameter
+															)
+														)
+													),
+												]
+												.filter(
+													(
+														( index ) => (
+																index
+															>=	0
+														)
+													)
+												)
+												.pop( )+1
+											)
+										]
+									);
+						}
+						else{
+							return	(
+										undefined
+									);
+						}
+					}
+				);
+
+				const resolveShellParameterValue = (
+					function resolveShellParameterValue( shellParameter, shortShellParameter, defaultValue ){
+						return	(
+										(
+												(
+														checkShellParameter(
+															(
+																shellParameter
+															),
+
+															(
+																shortShellParameter
+															)
+														)
+													===	true
+												)
+										)
+									?	(
+											getShellParameterValue(
+												(
+													shellParameter
+												),
+
+												(
+													shortShellParameter
+												)
+											)
+										)
+									:	(
+											defaultValue
+										)
+								);
+					}
+				);
+				//;	@end-template-engine-code-space;
+
+				const MODULE_DIRECTORY_PATH_SHELL_PARAMETER = (
+					"--moduleDirectoryPath"
+				);
+
+				const MODULE_DIRECTORY_PATH_SHORT_SHELL_PARAMETER = (
+					"--mdp"
+				);
+
+				const MODULE_VALUE_NAMESPACE_SHELL_PARAMETER = (
+					"--moduleValueNamespace"
+				);
+
+				const MODULE_VALUE_NAMESPACE_SHORT_SHELL_PARAMETER = (
+					"--mvn"
+				);
+
+				const MODULE_SCOPE_SHELL_PARAMETER = (
+					"--moduleScope"
+				);
+
+				const MODULE_SCOPE_SHORT_SHELL_PARAMETER = (
+					"--ms"
+				);
+
+				const MODULE_DESCRIPTION_SHELL_PARAMETER = (
+					"--moduleDescription"
+				);
+
+				const MODULE_DESCRIPTION_SHORT_SHELL_PARAMETER = (
+					"--md"
+				);
+
+				const AUTHOR_TITLE_NAMESPACE_SHELL_PARAMETER = (
+					"--authorTitleNamespace"
+				);
+
+				const AUTHOR_TITLE_NAMESPACE_SHORT_SHELL_PARAMETER = (
+					"--atn"
+				);
+
+				const AUTHOR_CONTACT_DETAIL_SHELL_PARAMETER = (
+					"--authorContactDetail"
+				);
+
+				const AUTHOR_CONTACT_DETAIL_SHORT_SHELL_PARAMETER = (
+					"--acd"
+				);
 
 				const createNodeShellModule = (
 					require( "./create-node-shell-module.js" )
 				);
 
 				const moduleDirectoryPath = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												MODULE_DIRECTORY_PATH_SHELL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												MODULE_DIRECTORY_PATH_SHELL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							MODULE_DIRECTORY_PATH_SHELL_PARAMETER
+						),
+
+						(
+							MODULE_DIRECTORY_PATH_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							process
 							.cwd( )
 						)
+					)
 				);
 
 				const moduleValueNamespace = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												MODULE_VALUE_NAMESPACE_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												MODULE_VALUE_NAMESPACE_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							MODULE_VALUE_NAMESPACE_SHELL_PARAMETER
+						),
+
+						(
+							MODULE_VALUE_NAMESPACE_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				const moduleScope = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												MODULE_SCOPE_SHELL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												MODULE_SCOPE_SHELL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							MODULE_SCOPE_SHELL_PARAMETER
+						),
+
+						(
+							MODULE_SCOPE_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				const moduleDescription = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												MODULE_DESCRIPTION_SHELL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												MODULE_DESCRIPTION_SHELL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							MODULE_DESCRIPTION_SHELL_PARAMETER
+						),
+
+						(
+							MODULE_DESCRIPTION_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				const authorTitleNamespace = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												AUTHOR_TITLE_NAMESPACE_SHELL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												AUTHOR_TITLE_NAMESPACE_SHELL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							AUTHOR_TITLE_NAMESPACE_SHELL_PARAMETER
+						),
+
+						(
+							AUTHOR_TITLE_NAMESPACE_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				const authorContactDetail = (
+					resolveShellParameterValue(
 						(
-								(
-										shellParameterList
-										.includes(
-											(
-												AUTHOR_CONTACT_DETAIL_PARAMETER
-											)
-										)
-									===	true
-								)
-						)
-					?	(
-							shellParameterList[
-								(
-									(
-										shellParameterList
-										.indexOf(
-											(
-												AUTHOR_CONTACT_DETAIL_PARAMETER
-											)
-										)
-									)+1
-								)
-							]
-						)
-					:	(
+							AUTHOR_CONTACT_DETAIL_SHELL_PARAMETER
+						),
+
+						(
+							AUTHOR_CONTACT_DETAIL_SHORT_SHELL_PARAMETER
+						),
+
+						(
 							undefined
 						)
+					)
 				);
 
 				return	(
